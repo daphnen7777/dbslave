3dd255dbeca467c77e91c7b9737ef7d1ae79287f
Dogun Kim <121995329+daphnen7777@users.noreply.github.com>
2024-12-09 18:39:44 +0900

    Update init.sql

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,3 +1,19 @@
 -- Create table and insert data with Korean characters
+DROP DATABASE IF EXISTS pratice_board;
+CREATE DATABASE pratice_board
+    DEFAULT CHARACTER SET utf8mb4
+    DEFAULT COLLATE utf8mb4_unicode_ci;
+	
+USE pratice_board;
+
+CREATE TABLE post (
+  `num` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `title` VARCHAR(50) NOT NULL,
+  `writer` VARCHAR(50) NOT NULL,
+  `content` TEXT NOT NULL,
+  `reg_date` DATETIME NOT NULL
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 CHANGE MASTER TO MASTER_HOST='10.10.10.13', MASTER_USER='root', MASTER_PASSWORD='root', MASTER_LOG_FILE='mysql-bin.000003', MASTER_LOG_POS=157;
+
 COMMIT;
